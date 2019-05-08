10e86d787612a15f876e4d57f2a7f96c96d59709
Guillermo <guipast25@gmail.com>
2019-05-08 18:44:57 +0200

    Minor Changes - Guille

--- BookingApp/src/main/java/com/es/eoi/service/BookingService.java
+++ BookingApp/src/main/java/com/es/eoi/service/BookingService.java
@@ -6,6 +6,7 @@ public class BookingService {
 	public static void createBooking() {
 		
 		//TODO logica de reservar un viaje
+		
 	}
 	
 	public static void findBookings() {
--- BookingApp/src/main/java/com/es/eoi/service/ClientService.java
+++ BookingApp/src/main/java/com/es/eoi/service/ClientService.java
@@ -1,11 +1,18 @@
 package com.es.eoi.service;
 
+import com.es.eoi.MainApp;
+import com.es.eoi.entity.Client;
+
 public class ClientService {
-	
-	
+
 	public static void findClients() {
+
+		// TODO logica de encontrar clientes
 		
-		//TODO logica de encontrar clientes
+	}
+
+	public static void addClient(Client client) {
+		MainApp.client.add(client);
 	}
 
 }
